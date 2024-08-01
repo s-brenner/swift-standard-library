636578467133ea0d1d954a1433b43dbf91a539f2
Scott Brenner <brenner.scott.980@icloud.com>
2024-07-31 21:01:58 -0600

    wip

--- Tests/SBStandardLibraryTests/FloatingPoint+Extensions.swift
+++ Tests/SBStandardLibraryTests/FloatingPoint+Extensions.swift
@@ -28,4 +28,24 @@ final class FloatingPointExtensionsTests: XCTestCase {
         XCTAssertEqual(Double(-2.3).clamped(to: 0...1), 0)
         XCTAssertEqual(Double(-4.2).clamped(to: -10...10), -4.2)
     }
+    
+    func testInterpolate() {
+        XCTAssertNil(4.interpolate(between: (5, 2), and: (10, 7)))
+        XCTAssertEqual(6.interpolate(between: (5, 2), and: (10, 7)), 3)
+        XCTAssertNil(4.interpolate(between: (10, 7), and: (5, 2)))
+        XCTAssertEqual(6.interpolate(between: (10, 7), and: (5, 2)), 3)
+        XCTAssertNil((-6).interpolate(between: (-5, -2), and: (10, 13)))
+        XCTAssertEqual(6.interpolate(between: (-5, -2), and: (10, 5.5)), 3.5)
+        XCTAssertNil(10.interpolate(between: (10, 10), and: (10, 20)))
+    }
+    
+    func testExtrapolate() {
+        XCTAssertEqual(4.extrapolate(from: (5, 2), and: (10, 7)), 1)
+        XCTAssertNil(6.extrapolate(from: (5, 2), and: (10, 7)))
+        XCTAssertEqual(4.extrapolate(from: (10, 7), and: (5, 2)), 1)
+        XCTAssertNil(6.extrapolate(from: (10, 7), and: (5, 2)))
+        XCTAssertEqual((-6).extrapolate(from: (-5, -2), and: (10, 13)), -3)
+        XCTAssertNil(6.extrapolate(from: (-5, -2), and: (10, 5.5)))
+        XCTAssertNil(11.extrapolate(from: (10, 10), and: (10, 20)))
+    }
 }
